e8f9568a744d10ef985c964db9f01a1ece84423a
amirAghayari <amiraghayari2119@gmail.com>
2025-07-13 12:45:24 +0330

    Add FirstVideo component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import NavBar from "./Sections/Navbar";
 import Hero from "./Sections/Hero";
+import FirstVideo from "./Sections/FirstVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,6 +11,7 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <FirstVideo />
     </main>
   );
 };
